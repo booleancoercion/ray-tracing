7a93731e4c8457b55db4ec979e5749248514d8e4
bool <booleancoercion@gmail.com>
2021-02-28 16:44:04 +0200

    Switched to using the image crate instead of ppm

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,12 @@
 pub mod collision;
 mod vec3;
 
-use std::io::Write;
-
 pub use collision::Ray;
 pub use vec3::Vec3;
 pub type Color = Vec3;
 pub type Point3 = Vec3;
 
-pub const ASPECT_RATIO: f64 = 16.0 / 9.0;
-
-pub fn write_color<W: Write>(
-    writer: &mut W,
-    pixel_color: Color,
-    samples_per_pixel: u32,
-) -> std::io::Result<()> {
+pub fn color_to_rgb(pixel_color: Color, samples_per_pixel: u32) -> image::Rgb<u8> {
     // divide the color by the number of samples and gamma-correct for gamma=2.0.
     let scale = 1.0 / samples_per_pixel as f64;
     let r = (pixel_color.x() * scale).sqrt();
@@ -22,11 +14,11 @@ pub fn write_color<W: Write>(
     let b = (pixel_color.z() * scale).sqrt();
 
     #[inline(always)]
-    fn intify(x: f64) -> u32 {
-        (256.0 * x.clamp(0.0, 0.999)) as u32
+    fn intify(x: f64) -> u8 {
+        (256.0 * x.clamp(0.0, 0.999)) as u8
     }
 
-    writeln!(writer, "{} {} {}", intify(r), intify(g), intify(b))
+    image::Rgb([intify(r), intify(g), intify(b)])
 }
 
 pub struct Camera {
@@ -36,28 +28,24 @@ pub struct Camera {
     vertical: Vec3,
 }
 
-impl Default for Camera {
-    fn default() -> Self {
-        let viewport_height = 2.0;
-        let viewport_width = ASPECT_RATIO * viewport_height;
-        let focal_length = 2.0;
-
-        let origin = Point3::new(0.0, 0.0, 2.0);
-        let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
-        let vertical = Vec3::new(0.0, viewport_height, 0.0);
-        let lower_left_corner =
-            origin - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0, 0.0, focal_length);
+impl Camera {
+    pub fn new(
+        origin: Point3,
+        lower_left_corner: Point3,
+        horizontal: Vec3,
+        vertical: Vec3,
+    ) -> Self {
+        assert!(!horizontal.near_zero());
+        assert!(!vertical.near_zero());
 
         Self {
             origin,
+            lower_left_corner,
             horizontal,
             vertical,
-            lower_left_corner,
         }
     }
-}
 
-impl Camera {
     pub fn get_ray(&self, u: f64, v: f64) -> Ray {
         Ray {
             origin: self.origin,
--- src/main.rs
+++ src/main.rs
@@ -2,85 +2,95 @@ use ray_tracing::collision::*;
 use ray_tracing::collision::{materials::*, objects::*};
 use ray_tracing::*;
 
+use image::ImageBuffer;
 use rand::Rng;
 
-use std::{
-    fs::OpenOptions,
-    io::{BufWriter, Write},
-    rc::Rc,
-};
+use std::io::Write;
+use std::rc::Rc;
 
 pub const ASPECT_RATIO: f64 = 16.0 / 9.0;
-pub const IMG_WIDTH: u32 = 800;
+pub const IMG_WIDTH: u32 = 400;
 pub const IMG_HEIGHT: u32 = (IMG_WIDTH as f64 / ASPECT_RATIO) as u32;
 pub const SAMPLES_PER_PIXEL: u32 = 100;
 pub const MAX_DEPTH: i32 = 50;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut rand = rand::thread_rng();
-    let file = OpenOptions::new()
-        .create(true)
-        .write(true)
-        .truncate(true)
-        .open("image.ppm")?;
-    let mut file = BufWriter::new(file);
 
     // World
-    let material_ground = Rc::new(Lambertian {
-        albedo: Color::new(0.8, 0.8, 0.0),
-    });
 
-    let material_left = Rc::new(Lambertian {
-        albedo: Color::new(0.7, 0.3, 0.3),
-    });
+    let world = {
+        let material_ground = Rc::new(Lambertian {
+            albedo: Color::new(0.8, 0.8, 0.0),
+        });
+        let material_left = Rc::new(Lambertian {
+            albedo: Color::new(0.7, 0.3, 0.3),
+        });
+        let material_center = Rc::new(Dielectric { ri: 1.3 });
+        let material_right = Rc::new(Metal::new(Color::new(0.05, 0.05, 0.05), 0.0));
 
-    let material_center = Rc::new(Dielectric { ri: 1.3 });
+        vec![
+            Sphere {
+                center: Point3::new(0.0, -100.5, -1.0),
+                radius: 100.0,
+                material: material_ground,
+            },
+            Sphere {
+                center: Point3::new(0.0, 0.0, -1.0),
+                radius: 0.5,
+                material: material_center.clone(),
+            },
+            /*
+            Sphere {
+                center: Point3::new(0.0, 0.0, -1.0),
+                radius: -0.4,
+                material: material_center,
+            },
+            */
+            Sphere {
+                center: Point3::new(-1.0, 0.0, -1.0),
+                radius: 0.5,
+                material: material_left.clone(),
+            },
+            Sphere {
+                center: Point3::new(1.0, 0.0, -1.0),
+                radius: 0.5,
+                material: material_right,
+            },
+        ]
+    };
 
-    let material_right = Rc::new(Metal::new(Color::new(0.05, 0.05, 0.05), 0.0));
+    // Camera
 
-    let world = vec![
-        Sphere {
-            center: Point3::new(0.0, -100.5, -1.0),
-            radius: 100.0,
-            material: material_ground,
-        },
-        Sphere {
-            center: Point3::new(0.0, 0.0, -1.0),
-            radius: 0.5,
-            material: material_center.clone(),
-        },
-        /*
-        Sphere {
-            center: Point3::new(0.0, 0.0, -1.0),
-            radius: -0.4,
-            material: material_center,
-        },
-        */
-        Sphere {
-            center: Point3::new(-1.0, 0.0, -1.0),
-            radius: 0.5,
-            material: material_left.clone(),
-        },
-        Sphere {
-            center: Point3::new(1.0, 0.0, -1.0),
-            radius: 0.5,
-            material: material_right,
-        },
-    ];
+    let camera = {
+        let viewport_height = 2.0;
+        let viewport_width = ASPECT_RATIO * viewport_height;
+        let focal_length = 2.0;
 
-    let camera = Camera::default();
+        let origin = Point3::new(0.0, 0.0, 2.0);
+        let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
+        let vertical = Vec3::new(0.0, viewport_height, 0.0);
+        let lower_left_corner =
+            origin - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0, 0.0, focal_length);
+
+        Camera::new(origin, lower_left_corner, horizontal, vertical)
+    };
 
     // Render
 
-    writeln!(&mut file, "P3\n{} {}\n255", IMG_WIDTH, IMG_HEIGHT)?;
+    let mut buf = ImageBuffer::new(IMG_WIDTH, IMG_HEIGHT);
 
     let mut stderr = std::io::stderr();
     for j in (0..IMG_HEIGHT).rev() {
         eprint!("\rScanlines remaining: {} ", j);
         stderr.flush()?;
 
+        let posy = IMG_HEIGHT - j - 1;
+
         let j = j as f64;
         for i in 0..IMG_WIDTH {
+            let posx = i;
+
             let i = i as f64;
 
             let mut pixel_color = Color::new(0.0, 0.0, 0.0);
@@ -93,9 +103,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 pixel_color += ray_color(&ray, &world[..], &mut rand, MAX_DEPTH);
             }
 
-            write_color(&mut file, pixel_color, SAMPLES_PER_PIXEL)?;
+            let rgb = color_to_rgb(pixel_color, SAMPLES_PER_PIXEL);
+            buf.put_pixel(posx, posy, rgb);
         }
     }
+
+    buf.save("output.png")?;
 
     eprintln!("\nDone.");
     Ok(())
