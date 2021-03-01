14e1b50510035c3bf7a316fb7d2d3acda64d82b2
boolean_coercion <booleancoercion@gmail.com>
2021-03-01 02:05:59 +0200

    made miniscule optimizations to sphere collision

--- src/collision/objects.rs
+++ src/collision/objects.rs
@@ -14,9 +14,22 @@ impl Hittable for Sphere {
     fn hit(&self, ray: &Ray, t_min: f64, t_max: f64) -> Option<Hit> {
         let oc: Vec3 = ray.origin - self.center;
 
+        let (a, half_b, c) = {
+            let [dx, dy, dz] = ray.direction.0;
+            let [ocx, ocy, ocz] = oc.0;
+            let r = self.radius;
+
+            (
+                dx * dx + dy * dy + dz * dz,
+                dx * ocx + dy * ocy + dz * ocz,
+                ocx * ocx + ocy * ocy + ocz * ocz - r * r,
+            )
+        };
+        /*
         let a = ray.direction.length_squared();
         let half_b = oc.dot(&ray.direction);
         let c = oc.length_squared() - self.radius * self.radius;
+        */
 
         #[allow(clippy::suspicious_operation_groupings)]
         let discriminant = half_b * half_b - a * c;
@@ -27,10 +40,13 @@ impl Hittable for Sphere {
 
         // Find the nearest root that lies in the acceptable range.
         let root = {
-            let mut root = (-half_b - sqrtd) / a;
-            if !(t_min..t_max).contains(&root) {
-                root = (-half_b + sqrtd) / a;
-                if !(t_min..t_max).contains(&root) {
+            let nhalf_b_a = -half_b / a;
+            let sqrtd_a = sqrtd / a;
+
+            let mut root = nhalf_b_a - sqrtd_a;
+            if root < t_min || t_max < root {
+                root = nhalf_b_a + sqrtd_a;
+                if root < t_min || t_max < root {
                     return None;
                 }
             }
